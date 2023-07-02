6ab01ebf3c934ec1f953605098a3f033863dcc46
Ethan Tai <ethan01tai@gmail.com>
2023-07-02 11:55:19 -0400

    added Board functionality, updated stylesheet and cells

---
+++ src/components/Board/cell.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./Board.module.scss";
+import {BoardNumbersByLetter, Colors} from "../../types";
+import classNames from "classnames";
+
+
+interface CellProps {
+    color: Colors;
+    x: string;
+    y: number;
+    cellClicked: (x: number, y: number) => void;
+    isAvailableForMove?: boolean;
+    isHavingFigure?: boolean;
+    isSelected?: boolean;
+}
+
+const Cell: React.FC<CellProps> = (props: CellProps) => {
+    return (
+        <li onClick={() => props.cellClicked(BoardNumbersByLetter[props.x], props.y)} id={`cell-${props.x}-${props.y}`} className={
+            classNames(styles.cell, {
+                [styles.cellWhite]: props.color === Colors.WHITE,
+                [styles.cellBlack]: props.color === Colors.BLACK,
+                [styles.availableCell]: props.isAvailableForMove && !props.isHavingFigure,
+                [styles.cellSelected]: props.isSelected,
+            })
+        }>
+            <div className={classNames(styles.cellCircle, {
+                [styles.cellCircleShow]: props.isAvailableForMove && !props.isHavingFigure
+            })}></div>
+        </li>
+    )
+}
+
+export  default Cell;
